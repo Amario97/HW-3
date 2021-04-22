d30e1e9c05f149012d6dcc7a82a4d400220d7ea6
Amario97 <marioihab_ti2o@hotmail.com>
2021-04-22 16:34:29 -0700

    HW 3 files

---
+++ main.js
@@ -0,0 +1,24 @@
+function myFunc() {
+   
+    let name = document.getElementById("name").value;
+    let city = document.getElementById("city").value
+    let paymentRef = document.getElementById("payref").value;
+
+    
+    if (name && city && paymentRef) {
+      
+      let div = document.getElementById("myDiv");
+      
+     
+      let h2 = document.getElementById("myH2");
+      
+      let list = document.createElement("ul");  
+      div.appendChild(list);                      
+
+     
+      let listItem = document.createElement("li");  
+      listItem.innerHTML = name + " " + city + " " + " " + paymentRef + "<br>"
+      list.appendChild(listItem);                   
+    } 
+  }
+
